20866f7394cfaa89450592ed37eb0d3fa3b7e4f6
Francis MudzungayirI <fmudzungayiri@gmail.com>
2025-02-09 19:29:52 +0200

    day 07

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, PasswordField
+from wtforms import StringField, TextAreaField, PasswordField, SelectField, EmailField
 from wtforms.validators import InputRequired
 
 
@@ -16,5 +16,16 @@ class GetProblem(FlaskForm):
 class LogIn(FlaskForm):
     
     username = StringField('Username', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
+    
+
+
+class NewUser(FlaskForm):
+    
+    firstname = StringField('First Name', validators=[InputRequired()])
+    surname = StringField('Last Name', validators=[InputRequired()])
+    useremail = EmailField('User email', validators=[InputRequired()])
+    system_role = SelectField('Choose role', choices=['Administrator','General User'], validators=[InputRequired()])
+    position = StringField('Work Position', validators=[InputRequired()])
     password = PasswordField('Password', validators=[InputRequired()])
     
--- static/js/main.js
+++ static/js/main.js
@@ -1,21 +1,29 @@
-alert('JS LOADED');
-
 
 /*** DASHBOARD VERTICAL TABS  ***/
-var vertical_tabs;
-var content;
+
+//var tab_btns = document.getElementsByClassName('tab-btn');
+
+// Object.keys(tab_btns).forEach(key => {
+//     let btn = tab_btns[key]
+//     console.log(btn.innerHTML)
+// });
+
+let tab_btns = document.querySelectorAll('.tab-btn');
+let tab_pane = document.querySelectorAll('.tab-pane');
 
 
-vertical_tabs = document.getElementsByClassName('vertical-tab')
-content = document.getElementsByClassName('tabs');
-tab_active = document.querySelector('.vertical-active')
+tab_btns.forEach(btn =>{
+    btn.addEventListener('click', ()=>{
 
-for(i = 0; i < vertical_tabs.length; i++){
-    vertical_tabs[i].addEventListener('click',()=>{
-        tab_active.classList.remove('vertical-active')
-        this.classList.add('vertical-active')
-    })
-}
-/*** CHANGING CONTENT ON VERTICAL TABS  CLICK  ***/
+        tab_btns.forEach(x =>  x.classList.remove('vertical-tab-active'));
+        tab_pane.forEach(pane => pane.classList.remove('pane-active'))
+        btn.classList.add('vertical-tab-active');
+
+        let targetTab = btn.getAttribute('data-tab');
+        console.log(targetTab)
+        document.getElementById(targetTab).classList.add('pane-active')
+    });
+});
+
 
 
